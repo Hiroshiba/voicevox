b83d27c6801a71752fb95f01b61bebf2580bc9ea
Hiroshiba <hihokaruta@gmail.com>
2024-09-09 11:38:47 +0900

    チェック用にeslint切る

--- src/components/Dialog/SettingDialog/FileNameTemplateDialog.stories.ts
+++ src/components/Dialog/SettingDialog/FileNameTemplateDialog.stories.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { userEvent, within, expect, waitFor } from "@storybook/test";
 
 import { Meta, StoryObj } from "@storybook/vue3";
