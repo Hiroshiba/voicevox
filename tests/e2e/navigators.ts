95aaea9d25c64a6e5ebf8e7ca900606de61a6ecb
Hiroshiba Kazuyuki <kazuyuki_hiroshiba@dwango.co.jp>
2025-03-21 21:22:40 +0900

    fix(tests): navigateToMainをnavigateToTalkに変更

--- tests/e2e/navigators.ts
+++ tests/e2e/navigators.ts
@@ -9,8 +9,8 @@ export async function gotoHome({ page }: { page: Page }) {
   });
 }
 
-export async function navigateToMain(page: Page) {
-  await test.step("初回起動時の確認を完了してメイン画面に移動", async () => {
+async function navigateToChoiceEditor(page: Page) {
+  await test.step("初回起動時の確認を完了してエディタ選択に移動", async () => {
     await expect(page.getByText("利用規約に関するお知らせ")).toBeVisible({
       timeout: 90 * 1000,
     });
@@ -21,6 +21,26 @@ export async function navigateToMain(page: Page) {
     await page.waitForTimeout(100);
     await page.getByRole("button", { name: "許可" }).click();
     await page.waitForTimeout(100);
+  });
+}
+
+export async function navigateToTalk(page: Page) {
+  await test.step("初回起動時の確認を完了してトーク画面に移動", async () => {
+    await navigateToChoiceEditor(page);
+    await expect(page.getByText("どちらに興味がありますか？")).toBeVisible();
+    await page.getByRole("button", { name: "トーク" }).click();
+  });
+}
+
+export async function navigateToSong(page: Page) {
+  await test.step("初回起動時の確認を完了してソング画面に移動", async () => {
+    await navigateToChoiceEditor(page);
+    await expect(page.getByText("どちらに興味がありますか？")).toBeVisible();
+    await page.getByRole("button", { name: "ソング" }).click();
+
+    // 見やすいようにスナップを1/8に変更
+    await page.getByLabel("スナップ").click();
+    await page.getByRole("option", { name: "1/8", exact: true }).click();
   });
 }
 
@@ -43,7 +63,7 @@ export async function toggleSetting(page: Page, settingName: string) {
 
 export async function navigateToHelpDialog(page: Page): Promise<Locator> {
   return await test.step("ヘルプダイアログの表示まで移動", async () => {
-    await navigateToMain(page);
+    await navigateToTalk(page);
     await page.waitForTimeout(100);
     await page.getByRole("button", { name: "ヘルプ" }).click();
     return getNewestQuasarDialog(page);
@@ -52,22 +72,10 @@ export async function navigateToHelpDialog(page: Page): Promise<Locator> {
 
 export async function navigateToSettingDialog(page: Page): Promise<Locator> {
   return await test.step("設定ダイアログの表示まで移動", async () => {
-    await navigateToMain(page);
+    await navigateToTalk(page);
     await page.waitForTimeout(100);
     await page.getByRole("button", { name: "設定" }).click();
     await getQuasarMenu(page, "オプション").click();
     return getNewestQuasarDialog(page);
-  });
-}
-
-export async function navigateToSong(page: Page) {
-  await test.step("ソング画面に移動", async () => {
-    await navigateToMain(page);
-    await expect(page.getByText("ソング")).toBeVisible();
-    await page.getByText("ソング").click();
-
-    // 見やすいようにスナップを1/8に変更
-    await page.getByLabel("スナップ").click();
-    await page.getByRole("option", { name: "1/8", exact: true }).click();
   });
 }
